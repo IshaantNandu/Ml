90bbaa22fb537a92ed2d803ae7324b8c72c841b7
Ishu <ishaant@outlook.com>
2024-12-21 20:53:31 +0530

    dd

--- src/main.py
+++ src/main.py
@@ -1,14 +1,23 @@
-#Data set reading
 import pandas
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import r2_score
+#Data set reading
+
 df = pandas.read_csv("../data/data.csv")
-X = df[['Weight', 'Volume']].values
-X = X.reshape(-1, 2)
+X = df[['Weight', 'Volume']].values.reshape(-1, 2)
 y = df['CO2']
 
-from sklearn import linear_model
+# Create polynomial features
+poly = PolynomialFeatures(degree=2)  # Adjust the degree as needed
+X_poly = poly.fit_transform(y)
 
-regr = linear_model.LinearRegression()
-regr.fit(X, y)
+# Model training
+model = LinearRegression()
+model.fit(X_poly, y)
 
-predictedCO2 = regr.predict([[2300, 1300]])
+new_data = [[2300, 1300]]  # New data point with Weight and Volume
+new_data_poly = poly.transform(new_data)
+predictedCO2 = model.predict(new_data_poly)
 print(predictedCO2)
+print(r2_score(y, model.predict(X_poly)))
